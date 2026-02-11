1647a0909fed0add482dae613c97a940e26cf868
DuongDuong19 <duongduong190906@gmail.com>
2026-02-11 16:19:26 +0000

    v2

--- Otp.java
+++ Otp.java
@@ -98,48 +98,4 @@ public class Otp {
             }
         }
     }
-
-    // public static void main(String[] args) {
-    //     Scanner scanner = new Scanner(System.in);
-    //     int attempts = 0;
-
-    //     String otp = generateOTP();
-        
-    //     System.out.println("Your OTP is: " + otp);
-
-    //     // verify OTP
-    //     boolean success = false;
-    //     for (int i = 1; i <= 5; i++) {
-    //         System.out.print("Enter OTP: ");
-    //         String input = scanner.nextLine().trim();
-
-    //         // check expired otp
-    //         /**
-    //          * if current time - created time >= 30s (EXPRIRE_TIME)
-    //          * -> reset time(created time) + overwrite old otp with new otp
-    //          */
-    //         if (System.currentTimeMillis() - createdTime >= EXPRIRE_TIME) {
-    //             otp = generateOTP();
-    //             createdTime = System.currentTimeMillis();
-    //             attempts = 0;
-
-    //             System.out.println("OTP expired. New OTP is: " + otp);
-    //         }
-
-    //         if (otp.equals(input)) {
-    //             System.out.println("Successful");
-    //             success = true;
-    //             break;
-    //         } else {
-    //             if (i < 5) {
-    //                 System.out.println("Wrong OTP. Attempts left: " + (5 - i));
-    //             }
-    //         }
-    //     }
-
-    //     if (!success) {
-    //         System.out.println("Too many attempts. Please try again later.");
-    //     }
-        
-    // }
 }
